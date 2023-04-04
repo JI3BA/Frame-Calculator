d12ae365d7cdb7ecb7f9a096e6dbb8a79b80b1a1
JI3BA <lyovkin_artyom@mail.ru>
2023-04-05 01:36:53 +0300

    added validation min max value for input number and some fix table component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Input.scss'
 import '../Input/InputBtn/InputBtn.scss'
 import InputBtn from "./InputBtn/InputBtn";
@@ -6,7 +6,22 @@ import InputBtn from "./InputBtn/InputBtn";
 const Input = (props) => {
     const config = props.configData
     const [inputValue, setInputValue] = useState(config.min)
+    const [disabledMinus, setDisabledMinus] = useState(false)
+    const [disabledPlus, setDisabledPlus] = useState(false)
 
+    useEffect(() => {
+        if(inputValue <= config.min){
+            setDisabledMinus(true)
+            setDisabledPlus(false)
+        }else if(inputValue >= config.max){
+            setDisabledPlus(true)
+            setDisabledMinus(false)
+        }else{
+            setDisabledPlus(false)
+            setDisabledMinus(false)
+        }
+        
+    }, [inputValue, config.min, config.max])
 
     const onClickBtn = (e) => {
         if(e.target.className.includes('plus')){
@@ -20,13 +35,11 @@ const Input = (props) => {
         <>
             <div className="input__container">
                 <input type="number" className="input" 
-                min={config.min}
-                max={config.max}
                 onChange={(e) => setInputValue(e.target.value)} 
                 onBlur={() => setInputValue(Number(inputValue).toFixed(1))}
                 value={(Number(inputValue))}/>
-                <InputBtn className='input__btn input__btn--plus' onClick={onClickBtn}>+</InputBtn>
-                <InputBtn className='input__btn input__btn--minus' onClick={onClickBtn}>-</InputBtn>
+                <InputBtn disabled={disabledPlus} className='input__btn input__btn--plus' onClick={onClickBtn}>+</InputBtn>
+                <InputBtn disabled={disabledMinus}className='input__btn input__btn--minus' onClick={onClickBtn}>-</InputBtn>
             </div>
            
         </> 
